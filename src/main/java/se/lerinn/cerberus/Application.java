95a43e916079b8937a63d40c1964d468f03b2a1e
Nikoleth <silberio50006@gmail.com>
2017-06-10 10:59:56 -0500

    Added the funQla method

--- src/main/java/se/lerinn/cerberus/Application.java
+++ src/main/java/se/lerinn/cerberus/Application.java
@@ -3,6 +3,10 @@ package se.lerinn.cerberus;
 public class Application {
 	
 	public static void main(String[] args) {
-		System.err.println("It's alive!!!!!");
+		funQla();
+	}
+	
+	public static void funQla() {
+		System.err.println("It's alive :D");
 	}
 }
