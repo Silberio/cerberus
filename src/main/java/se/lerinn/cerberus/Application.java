e68539040caff272b67e5749f0a3c01bf2687776
Erinn Bastias <silberio50006@gmail.com>
2017-06-10 17:53:21 +0200

    Changed output to error stream

--- src/main/java/se/lerinn/cerberus/Application.java
+++ src/main/java/se/lerinn/cerberus/Application.java
@@ -3,6 +3,6 @@ package se.lerinn.cerberus;
 public class Application {
 	
 	public static void main(String[] args) {
-		System.out.println("It's alive!!!!!");
+		System.err.println("It's alive!!!!!");
 	}
 }
